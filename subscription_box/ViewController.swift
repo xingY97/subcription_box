50bc17a68445da811ccb984c50e815263bbd379c
xingY97 <45300300+xingY97@users.noreply.github.com>
2021-03-02 14:33:14 -0500

    removed storyboard

---
+++ subscription_box/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  subscription_box
+//
+//  Created by X Y on 3/2/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
